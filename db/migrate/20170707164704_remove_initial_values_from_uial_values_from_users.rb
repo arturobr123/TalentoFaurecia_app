5e541e3ffda18a9f967b3235f93a153aa2f919cf
arturobr123 <tulinbr@hotmail.com>
2017-08-10 14:23:59 -0500

    init2

--- db/migrate/20170707164704_remove_initial_values_from_uial_values_from_users.rb
+++ db/migrate/20170707164704_remove_initial_values_from_uial_values_from_users.rb
@@ -10,7 +10,7 @@ class RemoveInitialValuesFromUialValuesFromUsers < ActiveRecord::Migration[5.0]
 
     remove_column :users, :language3, :text
     remove_column :users, :language3_percentage_reading, :integer
-    remove_column :users, :language3s_percentage_writing, :integer
+    remove_column :users, :language3_percentage_writing, :integer
 
     remove_column :users, :type_of_internship, :string
     remove_column :users, :status, :string
