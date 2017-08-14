ad43c8467017d942a49d01c75bf1f3213748a7c8
arturobr123 <tulinbr@hotmail.com>
2017-08-14 15:34:59 -0500

    init

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,2 @@
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:url] = 'https://s3.amazonaws.com/' + ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-east-2.amazonaws.com'
