7ac06eaf8e694ac8db25d0607903b51f41006638
arturobr123 <tulinbr@hotmail.com>
2017-08-17 12:28:56 -0500

    agosto17

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,8 +94,8 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => 'app74628088@heroku.com',
-    :password => 'televison1',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
 
