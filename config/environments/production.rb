772528c701dc3d0baa79bf3b833c97cee26a6d41
arturobr123 <tulinbr@hotmail.com>
2017-08-17 11:38:52 -0500

    agosto17

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,10 @@ Rails.application.configure do
     }
   }
 
+
+
+  config.action_mailer.default_url_options = { :host => "https://faurecia-students.herokuapp.com" }
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
