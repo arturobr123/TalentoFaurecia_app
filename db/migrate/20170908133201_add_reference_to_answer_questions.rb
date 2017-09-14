adc2035e6f1940ac962561702f8571f219a2f37b
arturobr123 <tulinbr@hotmail.com>
2017-09-14 10:18:23 -0500

    Septiembre13

--- db/migrate/20170908133201_add_reference_to_answer_questions.rb
+++ db/migrate/20170908133201_add_reference_to_answer_questions.rb
@@ -1,5 +1,5 @@
 class AddReferenceToAnswerQuestions < ActiveRecord::Migration[5.0]
   def change
-    add_reference :answer_questions, :question_first_filter, foreign_key: true
+    add_reference :answer_questions, :questions_first_filter, foreign_key: true
   end
 end
