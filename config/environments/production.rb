8c8a22cde184f4924b1f1c744357a38cae191bbd
arturobr123 <tulinbr@hotmail.com>
2017-09-16 20:37:36 -0500

    Septiembre16

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,6 @@ Rails.application.configure do
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
-      s3_protocol: 'https'
     }
   }
 
