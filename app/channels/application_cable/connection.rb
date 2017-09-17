e9c7512ed1618898703c320091de7479b7dd07f6
arturobr123 <tulinbr@hotmail.com>
2017-09-16 19:03:00 -0500

    Septiembre16

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -5,24 +5,35 @@ module ApplicationCable
   	def connect
   		self.current_admin = find_admin
   		puts "connect de connection"
+      logger.add_tags 'ActionCable', current_admin.email
   		
   	end
 
   	def find_admin
 
-  		admin_id = cookies.signed["admin.id"]
-      puts "FIND ADMIN!!"
-  		current_admin = Admin.find_by(id: admin_id)
+      if verified_admin = env['warden'].admin
+        verified_admin
+      else
+        reject_unauthorized_connection
+      end
 
-  		if current_admin
-  			current_admin
-  		else
-  			reject_unauthorized_connection
+  		#admin_id = cookies.signed["admin.id"]
+      #puts "FIND ADMIN!!"
+  		#current_admin = Admin.find_by(id: admin_id)
+
+  		#if current_admin
+  		#	current_admin
+  		#else
+  		#	reject_unauthorized_connection
   			
-  		end
+  		#end
+
+
   		
   	end
 
   	
   end
+
+
 end
